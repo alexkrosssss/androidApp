90067a0e785e3cc2b0727ad48d54fdf8ac1b5892
Alex-Kross <artemkirlanovia@gmail.com>
2022-10-24 19:26:42 +0300

    init commit

---
+++ app/src/main/java/com/android/labs/app/MainActivityTestModel.java
@@ -0,0 +1,15 @@
+package com.android.labs.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.labs.app.R;
+
+public class MainActivityTestModel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_equations);
+    }
+}
---
+++ testmodul/src/main/java/com/android/labs/testmodul/MainActivityTestModel.java
@@ -0,0 +1,13 @@
+package com.android.labs.testmodul;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityTestModel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.absolute_level);
+    }
+}
